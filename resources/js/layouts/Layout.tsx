21f62a06192e4968c0c660d23e7dd2fe92d77e21
Marcin <mrevib@gmail.com>
2025-05-26 14:12:08 +0200

    Corrected Layout

--- resources/js/layouts/Layout.tsx
+++ resources/js/layouts/Layout.tsx
@@ -1,14 +1,10 @@
-import {
-    AuditOutlined, BarChartOutlined,
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    UsergroupDeleteOutlined
-} from '@ant-design/icons';
+import NotificationMenu from '@/components/NotificationMenu';
+import UserMenu from '@/components/UserMenu';
+import { AuditOutlined, BarChartOutlined, MenuFoldOutlined, MenuUnfoldOutlined, UsergroupDeleteOutlined } from '@ant-design/icons';
 import { Head, Link, usePage } from '@inertiajs/react';
 import { Button, Layout, Menu, MenuProps, theme } from 'antd';
 import React, { useEffect, useState } from 'react';
-import UserMenu from '@/components/UserMenu';
-import NotificationMenu from '@/components/NotificationMenu';
+import { PageProps } from '@inertiajs/inertia';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -40,37 +36,40 @@ const items: MenuItem[] = [
 ];
 
 function getDefaultKeys() {
-    const openKeys: React.Key[] = []
+    const openKeys: React.Key[] = [];
 
     function findKeys(items: MenuItem[], keys: React.Key[] = []) {
-        return items.reduce<React.Key[]>((previousValue, currentValue) => {
-            if (currentValue.type === 'divider') return previousValue
+        return items.reduce<React.Key[]>(
+            (previousValue, currentValue) => {
+                if (currentValue.type === 'divider') return previousValue;
 
-            if (currentValue.children?.length) {
-                const res = findKeys(currentValue.children, keys)
+                if (currentValue.children?.length) {
+                    const res = findKeys(currentValue.children, keys);
 
-                if (res.length) {
-                    previousValue.push(...res)
-                    openKeys.push(currentValue.key)
+                    if (res.length) {
+                        previousValue.push(...res);
+                        openKeys.push(currentValue.key);
+                    }
                 }
-            }
 
-            if (currentValue.key.toString() === route('dashboard')) {
-                if (currentValue.key.toString() === location.href) {
-                    previousValue.push(currentValue.key)
+                if (currentValue.key.toString() === route('dashboard')) {
+                    if (currentValue.key.toString() === location.href) {
+                        previousValue.push(currentValue.key);
+                    }
+                } else if (location.href.split('?')[0] === currentValue.key.toString()) {
+                    previousValue.push(currentValue.key);
                 }
-            } else if (location.href.split('?')[0] === currentValue.key.toString()) {
-                previousValue.push(currentValue.key)
-            }
 
-            return previousValue
-        }, [...keys])
+                return previousValue;
+            },
+            [...keys],
+        );
     }
 
     return {
         openKeys,
         selectedKeys: findKeys(items),
-    }
+    };
 }
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children, title }) => {
@@ -82,7 +81,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, title }) => {
         token: { colorBgContainer },
     } = theme.useToken();
 
-    const pageProps = usePage<PageProps>()
+    const pageProps = usePage<PageProps>();
 
     const username = pageProps.props.auth.user.name;
 
@@ -94,15 +93,17 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, title }) => {
         <>
             <Head title={title} />
             <Layout style={{ minHeight: '100vh' }}>
-                <Sider trigger={null}
-                       collapsible
-                       collapsed={collapsed}
-                       breakpoint="lg"
-                       collapsedWidth="0"
-                       onBreakpoint={(broken) => {
-                           setCollapsed(broken);
-                       }}
-                       width={200}>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={collapsed}
+                    breakpoint="lg"
+                    collapsedWidth="0"
+                    onBreakpoint={(broken) => {
+                        setCollapsed(broken);
+                    }}
+                    width={200}
+                >
                     <div
                         className="demo-logo-vertical"
                         style={{
@@ -120,13 +121,15 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, title }) => {
                     <Menu theme="dark" defaultSelectedKeys={getDefaultKeys().selectedKeys} mode="inline" items={items} />
                 </Sider>
                 <Layout>
-                    <Header style={{
-                        padding: 0,
-                        background: colorBgContainer,
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center'
-                    }}>
+                    <Header
+                        style={{
+                            padding: 0,
+                            background: colorBgContainer,
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                    >
                         <Button
                             type="text"
                             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
@@ -140,9 +143,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, title }) => {
                         <NotificationMenu />
                         {username && <UserMenu username={username} />}
                     </Header>
-                    <Content style={{ margin: '16px 16px' }}>
-                        {children}
-                    </Content>
+                    <Content style={{ margin: '16px 16px' }}>{children}</Content>
                     <Footer style={{ textAlign: 'center' }}>Appgo ©{new Date().getFullYear()} Revi</Footer>
                 </Layout>
             </Layout>
