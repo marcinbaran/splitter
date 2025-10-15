fbca32dbdfcd3e4972b39e17bd923c27468b0db5
Marcin <mrevib@gmail.com>
2025-10-15 12:03:22 +0200

    Dont show size changer in table

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::prefix('settlements')->name('settlements.')->group(function () {
         Route::get('/', [SettlementController::class, 'index'])->name('index');
-        Route::get('/my', [SettlementController::class, 'myOrders'])->name('my');
         Route::get('/create', [SettlementController::class, 'create'])->name('create');
         Route::post('/store', [SettlementController::class, 'store'])->name('store');
         Route::get('/show/{settlement}', [SettlementController::class, 'show'])->name('show');
